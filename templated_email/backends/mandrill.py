0e2c7c255043d7ccc5042a8e4905557f2d43e2ca
Jan Michalski <jan.michalski@buzytor.com>
2015-09-23 15:01:47 +0200

    Add Mandrill backend

---
+++ templated_email/backends/mandrill.py
@@ -0,0 +1,31 @@
+import vanilla_django
+from django.core.mail import EmailMessage
+from django.conf import settings
+from django.utils.translation import ugettext as _
+
+# Make sure you have Mandrill as your email backend
+
+class TemplateBackend(vanilla_django.TemplateBackend):
+    def __init__(self, *args, **kwargs):
+        vanilla_django.TemplateBackend.__init__(self, *args, **kwargs)
+
+    def send(self, template_name, from_email, recipient_list, context, cc=None,
+            bcc=None, fail_silently=False, headers=None, template_prefix=None,
+            template_suffix=None, template_dir=None, file_extension=None,
+            **kwargs):
+
+        msg = EmailMessage(from_email=from_email, to=recipient_list)
+        msg.template_name = template_name
+        msg.global_merge_vars = context
+
+        if cc:
+            msg.cc = cc
+        if bcc:
+            msg.bcc = bcc
+
+        msg.use_template_subject = kwargs.get('use_template_subject', True)
+        msg.use_template_from = kwargs.get('use_template_from', True)
+        msg.async = kwargs.get('async', True)
+
+        msg.send()
+        return msg.mandrill_response
